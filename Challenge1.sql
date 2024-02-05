e37b3a1b05fa6c835041e826af76d73081d8a996
Charlotte Holcombe <97403042+c-holcombe1@users.noreply.github.com>
2024-02-05 17:12:19 +0000

    sql_challenges

---
+++ Challenge1.sql
@@ -0,0 +1,5 @@
+`data-analytics-bootcamp-363212.course14.gwz_nps`
+
+Challenge 1
+
+Task: Write a SQL query to select the orders_id, csat_product, and transporter from the table.
---
+++ Challenge2.sql
@@ -0,0 +1,5 @@
+`data-analytics-bootcamp-363212.course14.gwz_nps`
+
+Challenge 2
+
+Task: Write a SQL query to find all records where the global_note is greater than 7. This challenge introduces the concept of filtering data based on specific conditions.
