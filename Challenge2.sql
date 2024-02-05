bfb565dff3eb351e4f5de73637616308b10ef47c
jiayulee96 <jiayu@live.com.my>
2024-02-05 17:20:31 +0000

    Update Challenge2.sql
    added a filter where global_note>7

--- Challenge2.sql
+++ Challenge2.sql
@@ -2,4 +2,9 @@
 
 Challenge 2
 
-Task: Write a SQL query to find all records where the global_note is greater than 7. This challenge introduces the concept of filtering data based on specific conditions.
+Task: Write a SQL query to find all records where the global_note is greater than 7.
+ This challenge introduces the concept of filtering data based on specific conditions.
+SELECT 
+   *
+FROM `data-analytics-bootcamp-363212.course14.gwz_nps`
+WHERE global_note >7
